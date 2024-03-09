4e9479863cd0d1e12ca61d983d0ffb8ce8f01f6c
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-09 07:15:48 +0700

    create and compile first file with dart

---
+++ variable_and_datatypes/test/variable_and_datatypes_test.dart
@@ -0,0 +1,8 @@
+import 'package:variable_and_datatypes/variable_and_datatypes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
